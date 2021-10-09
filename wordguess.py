a93598b9f4a074113fbbc8bbf9e57fc5dfd9260a
apparatus-sachin <sraul864@gmail.com>
2021-10-09 14:59:27 -0700

    initial commit

---
+++ wordguess.py
@@ -0,0 +1,6 @@
+foundwords=("computer")
+user_guess=input("enter word: ")
+while user_guess !=foundwords:
+    user_guess=input("wrong guess!! please enter the correct word :")
+
+print("You Are Right !!!!")
